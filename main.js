a273e807d3314c68570f0f6b83a3cd91c7857d58
guesmi houssem <135331281+houssem-gasmi@users.noreply.github.com>
2023-12-14 15:28:42 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,156 @@
+let user={
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let str="1,2,3,EE,l,z,e,r,o,_,w,e,b,_,s,c,h,o,o,l,2,0,Z";
+
+let r=str.split(",").filter(str=>isNaN(parseInt(str))).map(str=>str.length?str.split("").pop():str).reduce((a,b,c,d)=>b!="_"&& c<d.length-true?a+b:a+" ");
+document.write(r);
+
+let t=
+{
+  nom:"idoudi",
+  prenom:"mouhamed";,
+  age:25;
+}
+eltllù_i
+ a=t.toString();
+document.write(a)*/
+
+
+  let user0={
+   "+q5dsq": "alii",
+  };
+
+  //document.write(user["+q5dsq"]);
+let a= "country";
+let damn={
+
+  country:"bikini",
+};
+//  document.write(damn[a]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let a=[2,4,6,8,10,12,1,3,5,7,9]
+//just i want the pair
+let res=a.filter(a=>!(a%2)).join(" ");
+document.write(res);
+
+
+
+
+let w="I Love FOOOD Code Too Playing Much";
+let res=w.split(" ").filter(w=>w.length<5).join(" ");
+document.write(res);
+let w="A13BS2Zx";
+var res=w.split("").filter(w=>Number(w)).map(w=>(w*w));
+
+document.write(res  +"</br>");    
+
+
+
+
+
+function reducer(a, c) {
+    var  returns = a + c;
+    document.write(
+      "</br>"+`accumulator: ${a}, currentValue: ${c}, returns: ${returns}`,
+    );
+    return returns;
+  }
+  array.reduce(reducer)
+
+document.write([15, 16, 17, 18, 19].reduce((a,b)=>a+b,10,));
+
+let reducer=(a,b)=>a+b  
+
+document.write(array.reduce(reducer));
+
+
+*/
+
